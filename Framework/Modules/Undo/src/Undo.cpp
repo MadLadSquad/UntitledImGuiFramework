4ab78e82597659ae91e77ccd4a666801dfc9fe5e
Madman10K <stanv.contact176@gmail.com>
2024-06-07 22:59:35 +0300

    Update imgui and the undo-redo algorithm

--- Framework/Modules/Undo/src/Undo.cpp
+++ Framework/Modules/Undo/src/Undo.cpp
@@ -3,6 +3,13 @@
 #include "Global.hpp"
 #include <Modules/Manager/ModulesManager.hpp>
 
+void eraseWhenOverBufferSize(std::deque<UImGui::Transaction*>& container, std::deque<UImGui::Transaction>& transactions, size_t i) noexcept
+{
+    for (size_t j = 0; j < container.size(); j++)
+        if (&transactions[i] == container[j])
+            container.erase(container.begin(), container.begin() + static_cast<std::remove_reference_t<decltype(container)>::difference_type>(j));
+}
+
 void UImGui::StateTracker::init() noexcept
 {
     transactionSize = Modules::data().maxTransactions;
@@ -28,30 +35,14 @@ void UImGui::StateTracker::pushAction(const UImGui::Transaction& transaction)
     if (transactions.size() == transactionSize)
     {
         // Destroying half of the buffer saves memory and performance for future transactions
-        for (uint32_t i = 0; i < (transactionSize/2); i++)
+        size_t size = transactionSize / 2;
+
+        for (size_t i = 0; i < size; i++)
         {
-            for (int j = 0; j < undoStack.size(); j++)
-            {
-                if (&transactions[i] == undoStack[j])
-                {
-                    undoStack[j] = nullptr;
-                    for (int m = 0; m <= j; m++)
-                        undoStack.pop_front();
-                }
-            }
-
-            for (int j = 0; j < redoStack.size(); j++)
-            {
-                if (&transactions[i] == redoStack[j])
-                {
-                    redoStack[j] = nullptr;
-                    for (int m = 0; m <= j; m++)
-                        redoStack.pop_front();
-                }
-            }
-
-            transactions.pop_front();
+            eraseWhenOverBufferSize(undoStack, transactions, i);
+            eraseWhenOverBufferSize(redoStack, transactions, i);
         }
+        transactions.erase(transactions.begin(), transactions.begin() + static_cast<decltype(transactions)::difference_type>(size));
     }
     transactions.push_back(transaction);
     undoStack.push_back(&transactions.back());
