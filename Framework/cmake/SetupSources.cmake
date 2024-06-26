56cecce9dea40c9e073676d360679c3f1c5ed690
Madman10K <stanv.contact176@gmail.com>
2024-06-27 00:34:25 +0300

    Possibly fix the spinners module linker error

--- Framework/cmake/SetupSources.cmake
+++ Framework/cmake/SetupSources.cmake
@@ -67,7 +67,7 @@ if (USE_KNOBS_MODULE)
 endif()
 
 if (USE_SPINNER_MODULE)
-    file(GLOB_RECURSE UGUI_SPINNER_HEAD "Framework/Modules/Spinners/*.h" ${IMGUI_SRC} "Framework/ThirdParty/imgui/*.cpp")
+    file(GLOB_RECURSE UGUI_SPINNER_HEAD "Framework/Modules/Spinners/*.h" ${IMGUI_SRC} "Framework/ThirdParty/imgui/core/*.cpp")
 endif()
 
 if (USE_TOGGLES_MODULE)
