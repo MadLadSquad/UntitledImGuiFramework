e861e270afc0c4327fd3be57ad30fce52b5960ee
Madman10K <stanv.contact176@gmail.com>
2026-01-07 04:25:13 +0200

    Remove references to now removed UVKBuildToolLib

--- create-project.sh
+++ create-project.sh
@@ -102,8 +102,6 @@ function compile()
   if [ "${windows}" == true ]; then
     cmake .. -G "Visual Studio ${VSShortVer} ${VSVer}" -DCMAKE_BUILD_TYPE=RELEASE || exit
     MSBuild.exe "${prjname}".sln -property:Configuration=Release -property:Platform=x64 -property:maxCpuCount="${cpus}" || exit
-
-    cp ../UVKBuildTool/build/Release/UVKBuildToolLib.dll . || exit
   elif [ "$2" == "wasm" ]; then
     emcmake cmake .. -DCMAKE_BUILD_TYPE=RELEASE || exit
     make -j "${cpus}" || exit
